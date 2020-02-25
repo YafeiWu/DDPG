7d46941306ebdee3456c1758051d0463342d217b
yafeiwu <yafeiw@opera.com>
2020-02-25 18:11:03 +0800

    Pendulum-v0

--- gym_ddpg.py
+++ gym_ddpg.py
@@ -3,20 +3,20 @@ from ddpg import *
 import gc
 gc.enable()
 
-ENV_NAME = 'InvertedPendulum-v1'
-EPISODES = 100000
+ENV_NAME = 'Pendulum-v0'
+EPISODES = 1000
 TEST = 10
 
 def main():
     env = filter_env.makeFilteredEnv(gym.make(ENV_NAME))
     agent = DDPG(env)
-    env.monitor.start('experiments/' + ENV_NAME,force=True)
+    # env.monitor.start('experiments/' + ENV_NAME,force=True)
 
-    for episode in xrange(EPISODES):
+    for episode in range(EPISODES):
         state = env.reset()
         #print "episode:",episode
         # Train
-        for step in xrange(env.spec.timestep_limit):
+        for step in range(env.spec.max_episode_steps):
             action = agent.noise_action(state)
             next_state,reward,done,_ = env.step(action)
             agent.perceive(state,action,reward,next_state,done)
@@ -25,19 +25,20 @@ def main():
                 break
         # Testing:
         if episode % 100 == 0 and episode > 100:
-			total_reward = 0
-			for i in xrange(TEST):
-				state = env.reset()
-				for j in xrange(env.spec.timestep_limit):
-					#env.render()
-					action = agent.action(state) # direct action for test
-					state,reward,done,_ = env.step(action)
-					total_reward += reward
-					if done:
-						break
-			ave_reward = total_reward/TEST
-			print 'episode: ',episode,'Evaluation Average Reward:',ave_reward
-    env.monitor.close()
+            total_reward = 0
+            for i in range(TEST):
+                state = env.reset()
+                for j in range(env.spec.max_episode_steps):
+                    #env.render()
+                    #direct action for test
+                    action = agent.action(state)
+                    state,reward,done,_ = env.step(action)
+                    total_reward += reward
+                    if done:
+                        break
+            ave_reward = total_reward/TEST
+            print('episode: {}; Evaluation Average Reward: {}'.format(episode,ave_reward))
+    # env.monitor.close()
 
 if __name__ == '__main__':
     main()
